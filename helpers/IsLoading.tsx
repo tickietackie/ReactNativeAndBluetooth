7de185153dba058e90c7725784a61ce8ea45eed9
tickietackie <kai.kuklok@gmail.com>
2021-09-15 18:43:44 +0200

    add is loading component

---
+++ helpers/IsLoading.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import BackgroundContainer from './BackgroundContainer';
+
+const Loading = function Loading() {
+  return (
+    <BackgroundContainer>
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator testID="Loading" size="large" color="red" />
+      </View>
+    </BackgroundContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+export default Loading;
