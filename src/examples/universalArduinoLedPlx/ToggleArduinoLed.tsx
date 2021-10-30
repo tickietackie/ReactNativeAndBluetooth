e8b6eb0cf89e9850469deb2a11f7e2e8505dcd7b
tickietackie <kai.kuklok@gmail.com>
2021-10-30 10:49:10 +0200

    Minor improvements and commenting

--- src/examples/universalArduinoLedPlx/ToggleArduinoLed.tsx
+++ src/examples/universalArduinoLedPlx/ToggleArduinoLed.tsx
@@ -33,18 +33,38 @@ const getBleManager = () => {
 };
 
 const manager = getBleManager();
-// const manager? = new BleManager();
 
-const connectToDevice = (device: Device, setLedStatus: Dispatch<SetStateAction<boolean>>) => {
+// connects to the device right after it was found to get the status of the LED
+const connectToDevice = (
+  device: Device,
+  setLedStatus: Dispatch<SetStateAction<boolean>>,
+  setArduinoFound: Dispatch<SetStateAction<Device | null>>,
+  setLoadingLedStatus: Dispatch<SetStateAction<boolean>>,
+  setToggleEnabled: Dispatch<SetStateAction<boolean>>
+) => {
   manager?.stopDeviceScan(); // stop scanning
   device
     .connect()
+    .then((connected) => {
+      console.log(`connected?: ${connected}`);
+      if (!connected) {
+        return device.connect();
+      }
+      return device;
+    })
     .then((connectedDevice) => {
-      console.log("connected");
+      console.log("connected to device to get the status");
       return connectedDevice.discoverAllServicesAndCharacteristics();
     })
     .then((connectedDevice) => {
-      console.log("return services");
+      // set found device after establishing a connection on web,
+      // so the connected status is preserved
+      // this is only relevant for web, as the device does not have to be requested again and
+      // the request window has not to be shown twice to the user in a a second attempt reconnecting
+      // this breaks functionality on native, so this can only be done for web
+      if (Platform.OS === "web") {
+        setArduinoFound(device);
+      }
       return connectedDevice.services();
     })
     .then((services) => {
@@ -52,33 +72,39 @@ const connectToDevice = (device: Device, setLedStatus: Dispatch<SetStateAction<b
       console.log("return characteristics");
       return services[0].characteristics();
     })
-    .then((characteristics) => {
-      console.log("tst");
-      return characteristics[0].read();
-    })
+    .then((characteristics) => characteristics[0].read())
     .then((characteristic) => {
       console.log("read value");
       console.log(characteristic.value);
       console.log(decode(characteristic.value ? characteristic.value : ""));
-      console.log("write value");
       if (!characteristic.value || parseInt(decode(characteristic.value), 10) === 0) {
         setLedStatus(false);
       } else {
         setLedStatus(true);
       }
+      setLoadingLedStatus(false);
+      setToggleEnabled(true);
     })
     .catch((error) => {
+      Alert.alert(`Fehler: ${error.message}`);
       console.error(error);
-      Alert.alert("Fehler", error.message);
     });
 };
 
+// lets the user toggle the LED by writing the opposite value of the status to the characteristic
 const toggle = (device: Device, setLedStatus: Dispatch<SetStateAction<boolean>>) => {
   manager?.stopDeviceScan(); // stop scanning
   device
-    .connect()
+    .isConnected()
+    .then((connected) => {
+      console.log(`connected?: ${connected}`);
+      if (!connected) {
+        return device.connect();
+      }
+      return device;
+    })
     .then((connectedDevice) => {
-      console.log("connected");
+      console.log("connected ... return discover and chars");
       return connectedDevice.discoverAllServicesAndCharacteristics();
     })
     .then((connectedDevice) => {
@@ -86,43 +112,42 @@ const toggle = (device: Device, setLedStatus: Dispatch<SetStateAction<boolean>>)
       return connectedDevice.services();
     })
     .then((services) => {
-      // console.log(services);
       console.log("return characteristics");
       return services[0].characteristics();
     })
     .then((characteristics) => {
-      console.log("tst");
+      console.log("reaturn read");
       return characteristics[0].read();
     })
     .then((characteristic) => {
-      console.log("read value");
-      console.log(characteristic.value);
-      console.log(decode(characteristic.value ? characteristic.value : ""));
+      console.log(`Read value: ${characteristic.value}`);
+      console.log(`Decoded value: ${decode(characteristic.value ? characteristic.value : "")}`);
       console.log("write value");
       if (!characteristic.value || parseInt(decode(characteristic.value), 10) === 0) {
-        console.log("write 1");
-        characteristic.writeWithResponse(encode("1"));
-        setLedStatus(true);
+        console.log("LED is off: write 1 to characteristic");
+        characteristic.writeWithResponse(encode("1")).then(() => setLedStatus(true));
       } else {
-        console.log("write 0");
-        characteristic.writeWithResponse(encode("0"));
-        setLedStatus(false);
+        console.log("LED is on: write 0 to characteristic");
+        characteristic.writeWithResponse(encode("0")).then(() => setLedStatus(false));
       }
     })
     .catch((error) => {
+      Alert.alert(`Fehler: ${error.message}`);
       console.error(error);
-      Alert.alert("Fehler", error.message);
     });
 };
 
 const ToggleArduinoLed = () => {
   const [isScanning, setIsScanning] = useState(false);
   const [ledStatus, setLedStatus] = useState(false);
+  const [loadingLedStatus, setLoadingLedStatus] = useState(false);
+  const [toggleEnabled, setToggleEnabled] = useState(false);
   const [list, setList] = useState<Device[] | []>([]);
   const [name, setName] = useState("test");
   const [arduinoFound, setArduinoFound] = useState<Device | null>(null);
   const [permissionGranted, setPermissionGranted] = useState(true);
 
+  // disconnect from the device when leaving the screen
   useFocusEffect(
     React.useCallback(
       () =>
@@ -136,22 +161,31 @@ const ToggleArduinoLed = () => {
     )
   );
 
+  // disconnect from the device and reset react native state
   const disconnect = () => {
     arduinoFound
       ?.cancelConnection()
       .then(() => {
+        setList([]);
+        setName("");
         setArduinoFound(null);
+        setLedStatus(false);
+        setToggleEnabled(false);
       })
-      .catch(() => alert("Failed to disconnect!"));
+      .catch(() => {
+        alert("Failed to disconnect! Maybe already disconnected.");
+        setArduinoFound(null);
+        setLedStatus(false);
+      });
   };
 
+  // React to changes on the bluetooth state itself
+  // not yet implemented in web
   React.useEffect(() => {
-    console.log("use effect");
     manager?.onStateChange((state: any) => {
       console.log("onStateChange");
       const subscription = manager?.onStateChange((state: any) => {
         if (state === "PoweredOn") {
-          // this && scanAndConnect();
           subscription.remove();
         }
       }, true);
@@ -159,6 +193,8 @@ const ToggleArduinoLed = () => {
     });
   }, [manager]);
 
+  // Get permission for Android
+  // in iOS and the Web permissions are handled by the browser or operating system, respectively
   const getPermissionOnAndroid = async () => {
     const granted = await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -179,6 +215,7 @@ const ToggleArduinoLed = () => {
     }
   };
 
+  // Get permission for Android on when navigating to this page the first time
   React.useEffect(() => {
     if (Platform.OS === "android") {
       (async () => {
@@ -187,6 +224,7 @@ const ToggleArduinoLed = () => {
     }
   }, []);
 
+  // Scan for the arduino by name and call the connect function if it was found
   const scanAndConnect = () => {
     if (isScanning) {
       manager?.stopDeviceScan();
@@ -195,39 +233,54 @@ const ToggleArduinoLed = () => {
       setIsScanning(false);
       setArduinoFound(null);
     } else {
-      manager?.startDeviceScan(null, null, (error, device) => {
-        setIsScanning(true);
-        if (error) {
-          // Handle error (scanning will be stopped automatically)
-          console.error(error);
-          return;
-        }
+      try {
+        manager?.startDeviceScan(null, null, (error, device) => {
+          setIsScanning(true);
+          if (error) {
+            // Handle error (scanning will be stopped automatically)
+            console.error(error);
+            alert(error.message);
+            return;
+          }
 
-        // Check if it is a device you are looking for based on advertisement data
-        // or other criteria.
-        console.log(device?.name, device?.localName, device?.id);
-        let deviceName = device && device.name ? device.name : "Unnamed";
-        const localName = device && device.localName ? device.localName : "Unnamed";
-        deviceName = deviceName === "Unnamed" ? localName : deviceName;
-        const id = device && device.id;
-        const alreadyInList = list.find((data) => data.id === id);
-        if (!alreadyInList && device) {
-          const newList: Device[] = list;
-          newList.push(device);
-          setList(newList);
-          setName(deviceName);
-        }
+          // Check if it is a device you are looking for based on advertisement data
+          // or other criteria.
+          console.log(device?.name, device?.localName, device?.id);
+          let deviceName = device && device.name ? device.name : "Unnamed";
+          const localName = device && device.localName ? device.localName : "Unnamed";
+          deviceName = deviceName === "Unnamed" ? localName : deviceName;
+          const id = device && device.id;
+          const alreadyInList = list.find((data) => data.id === id);
 
-        if (device?.localName === "LED" || device?.name === "LED" || device?.name === "Arduino") {
-          setArduinoFound(device);
-          setIsScanning(false);
-          connectToDevice(device, setLedStatus);
-          console.log("found device");
-          // Stop scanning as it's not necessary if you are scanning for one device.
-          manager?.stopDeviceScan();
-          // Proceed with connection.
-        }
-      });
+          // to show last found device, gather all devices not yet found
+          // and update the name of to newest one
+          if (!alreadyInList && device) {
+            const newList: Device[] = list;
+            newList.push(device);
+            setList(newList);
+            setName(deviceName);
+          }
+
+          if (device?.localName === "LED" || device?.name === "LED" || device?.name === "Arduino") {
+            console.log("Found device");
+            setArduinoFound(device);
+            setIsScanning(false);
+            setLoadingLedStatus(true);
+            // Stop scanning as it's not necessary if you are scanning for one device.
+            manager?.stopDeviceScan();
+            connectToDevice(
+              device,
+              setLedStatus,
+              setArduinoFound,
+              setLoadingLedStatus,
+              setToggleEnabled
+            );
+          }
+        });
+      } catch (error: any) {
+        alert(error.message);
+        console.error(error);
+      }
     }
   };
 
@@ -243,26 +296,40 @@ const ToggleArduinoLed = () => {
               title={title}
             />
           </View>
-          <Text>{`Last found device: ${name}`}</Text>
+
+          {isScanning && <Text>{`Last found device: ${name}`}</Text>}
 
           {isScanning ? (
             <Loading />
-          ) : arduinoFound ? (
-            <View style={styles.foundContainer}>
-              <View>
-                <Button
-                  title="Toggle LED"
-                  onPress={() => {
-                    toggle(arduinoFound, setLedStatus);
-                  }}
-                />
-              </View>
-              <View style={[styles.circle, { backgroundColor: ledStatus ? "green" : "white" }]} />
-            </View>
           ) : (
-            <View style={(styles.safeAreaContainer, styles.foundContainer)}>
-              <Text>Arduino not found yet</Text>
-            </View>
+            arduinoFound && (
+              <View style={styles.foundContainer}>
+                <View>
+                  <Button
+                    title="Toggle LED"
+                    disabled={!toggleEnabled}
+                    onPress={() => {
+                      toggle(arduinoFound, setLedStatus);
+                    }}
+                  />
+                </View>
+                <View
+                  style={[
+                    styles.circle,
+                    { backgroundColor: ledStatus ? "green" : "white" },
+                    { opacity: loadingLedStatus ? 0.5 : 1 }
+                  ]}
+                >
+                  {loadingLedStatus ? (
+                    <Loading />
+                  ) : (
+                    <Text style={{ color: ledStatus ? "white" : "grey" }}>
+                      {ledStatus ? "Turned on" : "Turned off"}
+                    </Text>
+                  )}
+                </View>
+              </View>
+            )
           )}
         </SafeAreaView>
       ) : (
@@ -296,29 +363,13 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly",
     width: "100%"
   },
-  item: {
-    padding: 10,
-    marginVertical: 8,
-    marginHorizontal: 10,
-    shadowColor: "#000000",
-    shadowOffset: {
-      width: 1,
-      height: 2
-    },
-    shadowOpacity: 0.75,
-    shadowRadius: 2,
-    elevation: 5,
-    borderRadius: 5,
-    minWidth: "80%"
-  },
-  title: {
-    fontSize: 12
-  },
   circle: {
     marginTop: 20,
     width: 100,
     height: 100,
-    borderRadius: 100 / 2
+    borderRadius: 100 / 2,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 
